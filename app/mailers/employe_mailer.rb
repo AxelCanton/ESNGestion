01032b933f3d7b7c09aefea2df8c2a4cf064a28f
Axel <axelcanton@hotmail.com>
2020-03-08 11:35:56 +0100

    a

--- app/mailers/employe_mailer.rb
+++ app/mailers/employe_mailer.rb
@@ -8,11 +8,5 @@ class EmployeMailer < ApplicationMailer
     @url="https://peaceful-wave-40701.herokuapp.com/"
     mail(to: params['email'], subject: 'Compte ESNGestion créé')
   end
-  def test
-    :subject => "hello",
-    :to  => "axelcanton01@gmail.com",
-    :from => axel.canton@etu.umontpellier.fr,
-    :html_body => "<strong>Hello</strong> dear Postmark user.",
-    :track_opens => "true")
-  end
+
 end
